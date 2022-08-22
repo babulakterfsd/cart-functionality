fbdb080978906e27c3675f84f24988dce83b7018
Babul Akter <babulakterfsd@gmail.com>
2022-08-22 23:52:52 +0600

    react, vite, eslint, prettier, yarn, context, react router dom, tailwind and all other things have been setup perfectly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,9 +3,7 @@ import React from 'react';
 function Home() {
     return (
         <div>
-            <h1 className="text-darkChocolate text-3xl">
-                Default Setup for vite, react and tailwind with yarn
-            </h1>
+            <h1 className="text-darkChocolate">hello</h1>
         </div>
     );
 }
